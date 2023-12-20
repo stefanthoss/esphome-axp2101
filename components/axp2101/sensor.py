120dc076deba80012f4c182781046f0f79640d11
Stefan Thoss <stefanthoss@users.noreply.github.com>
2023-12-19 21:56:15 -0800

    Initial commit

---
+++ components/axp2101/sensor.py
@@ -0,0 +1,58 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import i2c, sensor
+from esphome.const import (
+    CONF_ID,
+    CONF_BATTERY_LEVEL,
+    CONF_BRIGHTNESS,
+    UNIT_PERCENT,
+    ICON_BATTERY,
+    CONF_MODEL,
+)
+
+DEPENDENCIES = ["i2c"]
+
+axp2101_ns = cg.esphome_ns.namespace("axp2101")
+AXP2101Component = axp2101_ns.class_(
+    "AXP2101Component", cg.PollingComponent, i2c.I2CDevice
+)
+AXP2101Model = axp2101_ns.enum("AXP2101Model")
+
+MODELS = {
+    "M5CORE2": AXP2101Model.AXP2101_M5CORE2,
+}
+
+AXP2101_MODEL = cv.enum(MODELS, upper=True, space="_")
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(AXP2101Component),
+            cv.Required(CONF_MODEL): AXP2101_MODEL,
+            cv.Optional(CONF_BATTERY_LEVEL): sensor.sensor_schema(
+                unit_of_measurement=UNIT_PERCENT,
+                accuracy_decimals=1,
+                icon=ICON_BATTERY,
+            ),
+            cv.Optional(CONF_BRIGHTNESS, default=1.0): cv.percentage,
+        }
+    )
+    .extend(cv.polling_component_schema("60s"))
+    .extend(i2c.i2c_device_schema(0x77))
+)
+
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+    yield i2c.register_i2c_device(var, config)
+
+    cg.add(var.set_model(config[CONF_MODEL]))
+    if CONF_BATTERY_LEVEL in config:
+        conf = config[CONF_BATTERY_LEVEL]
+        sens = yield sensor.new_sensor(conf)
+        cg.add(var.set_batterylevel_sensor(sens))
+
+    if CONF_BRIGHTNESS in config:
+        conf = config[CONF_BRIGHTNESS]
+        cg.add(var.set_brightness(conf))
