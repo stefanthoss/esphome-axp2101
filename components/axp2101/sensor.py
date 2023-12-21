d96d4411acc75824c2644f42e4e0cda3087918c6
Stefan Thoss <stefanthoss@users.noreply.github.com>
2023-12-20 22:00:00 -0800

    Fix library

--- components/axp2101/sensor.py
+++ components/axp2101/sensor.py
@@ -34,7 +34,7 @@ def to_code(config):
     yield cg.register_component(var, config)
     yield i2c.register_i2c_device(var, config)
 
-    cg.add_library("https://github.com/lewisxhe/XPowersLib.git", None)
+    # cg.add_library("lewisxhe/XPowersLib", "0.2.1")
 
     cg.add(var.set_model(config[CONF_MODEL]))
 
