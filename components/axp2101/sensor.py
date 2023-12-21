c0d5cf484fe46f8b69579295b461023b30594e81
Stefan Thoss <stefanthoss@users.noreply.github.com>
2023-12-20 21:33:33 -0800

    Add library

--- components/axp2101/sensor.py
+++ components/axp2101/sensor.py
@@ -34,7 +34,10 @@ def to_code(config):
     yield cg.register_component(var, config)
     yield i2c.register_i2c_device(var, config)
 
+    cg.add_library("XPowersLib", "https://github.com/lewisxhe/XPowersLib.git")
+
     cg.add(var.set_model(config[CONF_MODEL]))
+
     if CONF_BATTERY_LEVEL in config:
         conf = config[CONF_BATTERY_LEVEL]
         sens = yield sensor.new_sensor(conf)
